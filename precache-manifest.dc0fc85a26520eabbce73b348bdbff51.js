622e562beaa9b074e3d9071a171de2943ae2085c
Yash Goyal <yagoyal@igategegdc.com>
2019-09-03 01:38:35 +0530

    Updates

---
+++ precache-manifest.dc0fc85a26520eabbce73b348bdbff51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae6946b5e11ddffd34337c9dcfd2483",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "09511a983df962cc6a56",
+    "url": "/monsters-rolodex/static/css/main.730276d8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "09511a983df962cc6a56",
+    "url": "/monsters-rolodex/static/js/main.51560cfc.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
